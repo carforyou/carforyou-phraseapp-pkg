6bcca27758500face52616a925c5c3196d7c7e89
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-01-08 09:15:04 +0100

    feat: read locale files paths from phraseapp config

---
+++ src/getLocalesPaths.js
@@ -0,0 +1,27 @@
+import fs from "fs"
+import path from "path"
+import yaml from "js-yaml"
+import glob from "glob"
+
+const configFilePath = ".phraseapp.yml"
+
+const getLocaleGlobs = () => {
+  try {
+    const phraseapConfig = yaml.safeLoad(fs.readFileSync(path.join(process.cwd(), configFilePath), "utf8"))
+    return phraseapConfig.phraseapp.pull.targets.map(target => {
+      return target.file.replace("<locale_name>", "*")
+    })
+  } catch {
+    console.log("Phraseapp config not found falling back to static/locales/*/*.json")
+
+    return [ "static/locales/*/*.json" ]
+  }
+}
+
+export default () => {
+  const localesGlobs = getLocaleGlobs()
+
+  return localesGlobs.reduce((acc, g) => {
+    return acc.concat(glob.sync(g))
+  }, [])
+}
